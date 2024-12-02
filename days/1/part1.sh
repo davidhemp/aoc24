af6cae2a9bab42c4089aba8d1d2609e49f0ce8c5
David Hempston <cyber_veio@hotmail.com>
2024-12-02 15:34:51 +0000

    Completed day 1 part 1

--- days/1/part1.sh
+++ days/1/part1.sh
@@ -1,1 +1,13 @@
-echo "Hello world"
+#!/bin/bash
+sort ../../data/day-1 | awk '{ print $1 }' > /tmp/left-sorted
+sort -k 2 ../../data/day-1 | awk '{ print $2 }' > /tmp/right-sorted
+
+TOTAL_DIFF=0
+while read -u 3 -r left && read -u 4 -r right ; do 
+    diff="$((left - right))"
+    if [[ $diff -lt 1 ]]; then 
+        diff="$(( diff * -1))"
+    fi
+    TOTAL_DIFF="$((TOTAL_DIFF + diff))"
+done 3</tmp/left-sorted 4</tmp/right-sorted 
+echo $TOTAL_DIFF
