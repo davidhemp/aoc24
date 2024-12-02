dcc9cb65eb065a9fd9fb738adf9b7ec1a406ca7d
David Hempston <cyber_veio@hotmail.com>
2024-12-02 17:19:03 +0000

    Increased day 2 examples and using new loop

--- days/2/part1.sh
+++ days/2/part1.sh
@@ -4,24 +4,43 @@ TOTAL=0
 while read -u 3 -r report ; do 
     SAFE=1
     DIRECTION=0
-    for i in $(echo $report | awk '{ print $1-$2,$2-$3,$3-$4,$4-$5}'); do
-        # Set the direction if it is not set
+    START=1
+    n=0
+    nn=0
+    for level in $report; do
+        # Skip the first level
+        if [[ $START -eq 1 ]]; then
+            n=$level
+            START=0
+            continue
+        fi
+        
+        # Increment the levels
+        nn="$n"
+        n="$level"
+        # Calculate the difference between two adjacent levels
+        DIFF=$(( nn - n ))
+
+        # Check direction or set if it is not set yet
         if [[ $DIRECTION -eq 0 ]]; then
-            if [[ $i -lt 0 ]]; then
+            if [[ DIFF -lt 0 ]]; then
                 DIRECTION=-1
-            elif [[ $i -gt 0 ]]; then
+            elif [[ DIFF -gt 0 ]]; then
                 DIRECTION=1
             fi
-        elif [[ $DIRECTION -eq -1 ]] && [[ $i -gt 0 ]]; then
+        elif [[ $DIRECTION -eq -1 ]] && [[ DIFF -gt 0 ]]; then
+            ERR="DIRECTION CHANGED"
             SAFE=0
-        elif [[ $DIRECTION -eq 1 ]] && [[ $i -lt 0 ]]; then
+        elif [[ $DIRECTION -eq 1 ]] && [[ DIFF -lt 0 ]]; then
+            ERR="DIRECTION CHANGED"
             SAFE=0
         fi
 
         # Switch to absolute value for checking the difference
-        i="${i#-}"
+        DIFF="${DIFF#-}"
         # Any two adjacent levels differ by at least one and at most three.
-        if [[ $i -eq 0 ]] || [[ $i -gt 3 ]]; then
+        if [[ $DIFF -eq 0 ]] || [[ $DIFF -gt 3 ]]; then
+            ERR="Out of range ${DIFF}"
             SAFE=0
         fi
     done
@@ -29,7 +48,7 @@ while read -u 3 -r report ; do
         TOTAL=$((TOTAL + 1))
         echo "SAFE"
     else
-        echo "UNSAFE"
+        echo "UNSAFE: $ERR $report"
     fi
 done 3< ../../data/day-2-example
 
