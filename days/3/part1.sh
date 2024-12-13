0535f623224f31df578a074a057e037c4451e2c7
David Hempston <cyber_veio@hotmail.com>
2024-12-13 12:06:10 +0000

    Correct part 1

--- days/3/part1.sh
+++ days/3/part1.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-cat ../../data/day-3-example
+
+if [[ $1 == "" ]]; then
+    DATA="../../data/day-3"
+elif [[ $1 == "test" ]]; then
+    DATA="../../data/day-3-example"
+else
+    DATA=$1  
+fi
+
+egrep -o "mul\([0-9]{1,3}\,[0-9]{1,3}\)" $DATA > /tmp/filtered_mul
+
+total=0
+for line in $(cat /tmp/filtered_mul); do
+    IFS=',' read -r -a array <<< $(echo $line | tr -d 'mul()')
+    total=$((total + array[0] * array[1]))
+done
+echo $total
